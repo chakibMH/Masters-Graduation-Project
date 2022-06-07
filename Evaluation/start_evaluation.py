6b7aed9f0a29982b013d318958363e3952c96907
Purple Dragon <37756864+Serine07@users.noreply.github.com>
2022-06-07 11:10:00 +0100

    update

--- Evaluation/start_evaluation.py
+++ Evaluation/start_evaluation.py
@@ -25,30 +25,9 @@ with open("original_queries/original_batch_100_concepts_with_deff", "rb") as f:
     queries = pickle.load(f)
 
 
-queries2 = ['cluster analysis', 'Image segmentation', 'Parallel algorithm', 'Monte Carlo method',
-            'Convex optimization', 'Dimensionality reduction', 'Facial recognition system', 
-            'k-nearest neighbors algorithm', 'Hierarchical clustering', 'Automatic summarization',
-            'Dynamic programming', 'Genetic algorithm', 'Human-computer interaction', 'Categorial grammar', 
-            'Semantic Web', 'fuzzy logic', 'image restoration', 'generative model', 'search algorithm',
-            'sample size determination', 'anomaly detection', 'sentiment analysis', 'semantic similarity',
-            'world wide web', 'gibbs sampling', 'user interface', 'belief propagation', 'interpolation', 
-            'wavelet transform', 'transfer of learning', 'topic model', 'clustering high-dimensional data', 
-            'game theory', 'biometrics', 'constraint satisfaction', 'combinatorial optimization', 'speech processing',
-            'multi-agent system', 'mean field theory', 'social network', 'lattice model', 'automatic image annotation',
-            'computational geometry', 'Evolutionary algorithm', 'web search query', 'eye tracking', 'query optimization',
-            'logic programming', 'Hyperspectral imaging', 'Bayesian statistics', 'kernel density estimation',
-            'learning to rank', 'relational database', 'activity recognition', 'wearable computer', 'big data', 
-            'ensemble learning', 'wordnet', 'medical imaging', 'deconvolution', 'Latent Dirichlet allocation', 
-            'Euclidean distance', 'web service', 'multi-task learning', 'Linear separability', 'OWL-S',
-            'Wireless sensor network', 'Semantic role labeling', 'Continuous-time Markov chain', 
-            'Open Knowledge Base Connectivity', 'Propagation of uncertainty', 'Fast Fourier transform', 
-            'Security token', 'Novelty detection', 'semantic grid', 'Knowledge extraction', 
-            'Computational biology', 'Web 2.0', 'Network theory', 'Video denoising', 'Quantum information science',
-            'Color quantization', 'social web', 'entity linking', 'information privacy', 'random forest', 
-            'cloud computing', 'Knapsack problem', 'Linear algebra', 'batch processing', 'rule induction', 
-            'Uncertainty quantification', 'Computer architecture', 'Best-first search', 'Gaussian random field',
-            'Support vector machine', 'ontology language', 'machine translation', 'middleware', 'Newton\'s method']
-
+with open("original_queries/original_batch_100_concepts.pkl", "rb") as f:
+    queries2 = pickle.load(f)
+    
 #############################################################################################
 
 
