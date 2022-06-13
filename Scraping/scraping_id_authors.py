8f6b52ddb7f8b9a7ad574b02c6e2a0b00f52dce8
Purple Dragon <37756864+Serine07@users.noreply.github.com>
2022-06-13 19:00:13 +0100

    update

--- Scraping/scraping_id_authors.py
+++ Scraping/scraping_id_authors.py
@@ -75,8 +75,14 @@ def get_colleagues(id_au, n=5):
     
     soup = BeautifulSoup(src, "lxml")
     
-    try:
     
+    
+    try:
+        list_ids=[]
+        for a in soup.find_all('a', {"contrib-link"},href=True):
+            list_ids.append(a['href']) 
+            
+            
         authors  = soup.find("ul",{"rlist results-list"})
         respon_text = ""
         for li in authors.find_all("li"):
@@ -100,10 +106,15 @@ def get_colleagues(id_au, n=5):
     
     size = len(l_names)
     
+    
     if size >= 5 :
         l_names = l_names[:5]
+        list_ids = list_ids[:5]
+    l_f=[]   
+    for n,i in zip(l_names,list_ids):
+        l_f.append((n,i))
     
-    return l_names
+    return l_f
 
 
 
@@ -125,7 +136,7 @@ def get_all_authors_colleagues(list_authors,n=5):
     
 
 
-list_authors=["Laura Dietz","Kristina A Keesom"]
+
 
 def execut(list_authors,n):
     start = time.time()
@@ -135,16 +146,52 @@ def execut(list_authors,n):
     
     return ll
 
-list_authors = list_top_500
+# list_authors=list_names_top_5_prolific
 
-ll=execut(list_authors,5)
+# list_authors = top_500[:2]
+
+# ll=execut(list_authors,5)
+
+# list_author_id=[]
+# for d in top_500_data:
+#     list_author_id.append([d[0],d[1]])
+
+# import pickle
+# with open('top_500_data.pkl', 'wb') as f:
+#    pickle.dump(data, f)
+
+# with open('top_500_data.pkl', 'rb') as f:
+#    data_ = pickle.load(f)
+
+
+
+import pickle
+with open('get_ids_in_dict.pkl', 'rb') as f:
+   get_ids_in_dict= pickle.load(f)
+
+get_ids_in_dict = list(get_ids_in_dict)
+
+get_ids_in_dict_ = get_ids_in_dict[100:1000]
+get_ids_in_dict_ = get_ids_in_dict[1000:2000]
+get_ids_in_dict_ = get_ids_in_dict[2000:3000]
+get_ids_in_dict_ = get_ids_in_dict[3000:4000]
+
+
+
+start = time.time()
+l_ids = get_all_authors_ids(get_ids_in_dict_)
+end = time.time()
+print("time: ",(end - start)/60," min")
+
+
+
+import pickle
+with open('IDs/id_authors_0_100.pkl', 'wb') as f:
+   pickle.dump(l_ids, f)
 
 
 
 
 import pickle
-with open('top_500_data.pkl', 'wb') as f:
-   pickle.dump(data, f)
-
-with open('top_500_data.pkl', 'rb') as f:
-   data_ = pickle.load(f)
+with open('IDs/id_authors_0_100.pkl', 'rb') as f:
+   ids= pickle.load(f)
