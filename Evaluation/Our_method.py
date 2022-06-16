361a74fbcc75997988aedc9ec7a0c3acbbc2d872
Purple Dragon <37756864+Serine07@users.noreply.github.com>
2022-06-16 10:48:51 +0100

    update

--- Evaluation/Our_method.py
+++ Evaluation/Our_method.py
@@ -400,7 +400,68 @@ def get_relevant_authors(file_name,strategy = 'sum',norm=True, deff_type="mean",
 ########################################################
 
 
+def get_relevant_authors_without_def(file_name,strategy = 'sum',norm=True, transform_to_score_before=True):
 
+    results_all_queries = pd.DataFrame()
+    
+    d_all_query = {}
+    i=1
+    l = len(queries2)
+    for q in queries2:
+        print('current query: ',q,' [{}/{}'.format(i,l)) 
+        # score_authors_dict = get_relevant_experts(q, sen_index, data, 
+        #                                           authors, embedder, strategy,norm,
+        #                                           transform_to_score_before, 2000)
+        
+        score_authors_dict = get_relevant_experts(q, sen_index, data, authors, embedder
+                                                  ,strategy, norm ,  transform_to_score_before)
+        
+        
+        d_all_query[q] = score_authors_dict
+        i+=1
+    df = pd.DataFrame(d_all_query)
+    all_authors = authors.id.values
+    l = list(df.index)
+    to_drop = []
+    for i in l:
+        if i not in all_authors:
+            # to drop
+            to_drop.append(i)
+    
+    df.drop(to_drop, inplace=True)
+    df.to_csv(file_name+".csv")
+    
+
+    
+    
+    relvents_auths = pd.read_csv(file_name+".csv")
+    
+    relvents_auths = relvents_auths.rename(columns={relvents_auths.columns[0]: 'id'})
+    
+    list_ids_relevant=relvents_auths["id"].tolist()
+    
+    
+    def retrieve_author_tags_new(authors, author_id):
+      
+        author_id= int(author_id)
+        try:
+            return ast.literal_eval(authors[authors.id == author_id].tags.values[0])
+        except:
+            return {}
+    
+    
+    for n in list_ids_relevant:
+        tags = [t['t'].lower() for t in retrieve_author_tags_new(authors,n)]
+        #print("* tags  : ",tags,"// id : ",n)
+        
+        if tags:
+            b=1
+        else:
+            relvents_auths.drop(relvents_auths.index[relvents_auths['id'] == n], inplace=True)
+    
+    relvents_auths.to_csv(file_name+'_new.csv', index=False)
+    
+    
 
 def save_files(file_path):
 
