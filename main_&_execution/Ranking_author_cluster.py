f35bbbf0dc3e1b16426d33117e8206fa42cde8de
chakibMH <84680800+chakibMH@users.noreply.github.com>
2022-09-10 10:40:21 +0100

    optimized

--- main_&_execution/Ranking_author_cluster.py
+++ main_&_execution/Ranking_author_cluster.py
@@ -12,6 +12,7 @@ from custom_faiss_indexer import len_paper,load_index
 from sklearn.preprocessing import normalize
 from def_cleaning import clean_def
 import pandas as pd
+import pickle
 # from distance_functions import dist2sim
 
 def dist2sim(d):
@@ -508,9 +509,23 @@ def get_relevant_experts_multi_index(queries, list_index_path, papers,
     
     return final_score_each_query, final_papers_of_expertise
         
+def str_to_list(list_str):
+    
+    def remove_extra_spaces(txt):
+    
+        return " ".join(txt.split())
+    
+    list_str = remove_extra_spaces(list_str)
+    list_str = list_str.replace(" ",",")
+    if list_str[1] == ',':
+        list_str = list_str[:1] + list_str[2:]
+     
+    list_float = ast.literal_eval(list_str)
+    
+    return list_float
 
 
-def authors_expertise_to_paper_v2(p_id, papers) :
+def authors_expertise_to_paper_v2(p_id, papers, profiles, loc_embs) :
     """
     
     return 
@@ -529,7 +544,140 @@ def authors_expertise_to_paper_v2(p_id, papers) :
 
     """
     
-    return True
+    dict_expertise = {}
+    #read the mean embedding of the paper
+    
+    # search for the rigth embedding file
+    
+
+    
+    # {"pid":N}
+    n = loc_embs[p_id]
+    # loc determined
+    emb_file = "dependencies/docs_embedding/emb"+str(n)+".csv"
+    #print("he*****re")
+    #print(emb_file)
+    emb = pd.read_csv(emb_file)
+    #print("bbbbbbbbbbbbbbbbbbbbb")
+    v_doc = emb.loc[emb.id_paper == p_id].mean_embedding.values[0]
+
+    
+    del(emb)
+    #print("ccccccccccccccccccccccccccccc")
+    v_doc = str_to_list(v_doc)
+    
+    v_doc = np.array(v_doc)
+    
+    # normalize v_doc
+    v_doc = np.float32(normalize(np.array([v_doc]))[0])
+    
+    # calculate the distance with all authors of this paper
+    
+    # determining the authors for this document
+    
+    authors = papers.loc[papers.id_paper == p_id].authors.values[0]
+    
+    authors = ast.literal_eval(authors)
+    
+    for da in authors:
+        
+        
+        a_name = da['name']
+        
+        centroid = profiles[a_name]['centroid']
+        
+        # normalize v_doc
+        
+        centroid = np.float32(normalize(np.array([centroid]))[0])
+        
+        #calculate the distance in [1:3]
+        
+        dist = np.linalg.norm(centroid-v_doc)+1
+        
+        # use id author ??
+        
+        a_id = da['id']
+        
+        dict_expertise[a_id] = dist
+    
+    
+    #print("ddddddddddddddddddddddddddddddddd")
+    
+    return dict_expertise
+
+
+def authors_expertise_to_paper_v3(p_id, papers, profiles, df_embs) :
+    """
+    
+    return 
+
+    Parameters
+    ----------
+    p_id : str
+        DESCRIPTION.
+    papers : pd.Dataframe
+        DESCRIPTION.
+
+    Returns
+    -------
+    bool
+        DESCRIPTION.
+
+    """
+    
+    dict_expertise = {}
+    #read the mean embedding of the paper
+    
+    # search for the rigth embedding file
+
+    v_doc = df_embs.loc[df_embs.id_paper == p_id].mean_embedding.values[0]
+
+    
+    #print("ccccccccccccccccccccccccccccc")
+    v_doc = str_to_list(v_doc)
+    
+    v_doc = np.array(v_doc)
+    
+    # normalize v_doc
+    v_doc = np.float32(normalize(np.array([v_doc]))[0])
+    #print("eeeeeeeeeeeeeeeeeeeeeeeee")
+    
+    # calculate the distance with all authors of this paper
+    
+    # determining the authors for this document
+    
+    authors = papers.loc[papers.id_paper == p_id].authors.values[0]
+    
+    authors = ast.literal_eval(authors)
+    
+    for da in authors:
+        
+        
+        a_name = da['name']
+        
+        if a_name in profiles.keys():
+        
+            centroid = profiles[a_name]['centroid']
+        
+        # normalize v_doc
+        
+        centroid = np.float32(normalize(np.array([centroid]))[0])
+        
+        #calculate the distance in [1:3]
+        
+        dist = np.linalg.norm(centroid-v_doc)+1
+        
+        # use id author ??
+        
+        a_id = da['id']
+        
+        dict_expertise[a_id] = dist
+        #print("auth expertise",dict_expertise[a_id])
+    
+    
+    #print("ddddddddddddddddddddddddddddddddd")
+    
+    return dict_expertise
 
 
 def query_with_deff(query, deff_type, embedder, sen_index, k, a=0.7, b=0.3):
@@ -632,13 +780,14 @@ def get_search_result(query, sen_index, embedder,
 
 
 def get_scores(df_res, papers,authors, dist_score_cluster = False,
-               norm = False, ):
+               norm = False, profiles = None, loc_embs = None, df_embs = None):
     
     print("start deteermining score...")
     
     
     ids_of_sim_papers = list(df_res.index)
     
+    global not_found
     
     #cle : id auteur
     # val : sum
@@ -648,15 +797,22 @@ def get_scores(df_res, papers,authors, dist_score_cluster = False,
     
     existing_ids = papers.id_paper.values
     
+
+    di = 1
+    td = len(ids_of_sim_papers)
     for p_id in ids_of_sim_papers:
+        print("doc . [{}/{}]".format(di,td))
+        di+=1
+        #td+=1
         
-        if p_id in existing_ids:
+        if (p_id in existing_ids) and (p_id not in df_embs.keys()):
         
             # NEW !
             if dist_score_cluster == True:
-            
+                print("*"*50)
                 
-                dict_expertise = authors_expertise_to_paper_v2(p_id, papers,authors)
+                #dict_expertise = authors_expertise_to_paper_v2(p_id, papers, profiles, loc_embs)
+                dict_expertise = authors_expertise_to_paper_v3(p_id, papers, profiles, df_embs)
             # {id_auth:Dist_Centroid_D: pour chaque auth de p_id}
             # END NEW
             
@@ -672,16 +828,22 @@ def get_scores(df_res, papers,authors, dist_score_cluster = False,
             
             sim_Q_D = df_res.loc[p_id]
             
+            ia = 1
+            ta = len(auth_of_p_id)
+            
             for a in auth_of_p_id:
-                
-    
+                print(" "*10,"auth . [{}/{}]".format(ia,ta))
+                ia+=1
+                #ta+=1
                 
                 # NEW !
                 if dist_score_cluster == True:
                     
-                    dist_D_A = dict_expertise[a]
+                    #test if aid is in the list of ids ??
                     
-                    sim_D_A = dist2sim(dist_D_A)
+                    sim_D_A = dict_expertise[a]
+                    
+                    #sim_D_A = dist2sim(dist_D_A)
                     
                  # END NEW  
                             
@@ -702,17 +864,23 @@ def get_scores(df_res, papers,authors, dist_score_cluster = False,
                     # papers used in expertise
                     
                     papers_of_expertise[a].append(p_id)
+                    #print("+"*15," added socre for this doc before : ", math.exp(sim_Q_D))
+                    #print("+"*15," added socre for this doc after : ", math.exp(sim_Q_D/sim_D_A))
     
-                    score_authors_dict[a] += math.exp(sim_D_A * sim_Q_D)
+                    #print("/"*5,"  socre for this auth before : ", score_authors_dict[a])
+                    score_authors_dict[a] += math.exp(sim_Q_D / sim_D_A)
+                    #print("/"*5,"  socre for this auth after : ", score_authors_dict[a])
                     # score_authors_dict[a] += math.exp(sim_Q_D)
                     
                 else:
+                    #print("+"*15," added socre for this doc before : ", math.exp(sim_Q_D))
+                    #print("+"*15," added socre for this doc after : ", math.exp(sim_Q_D/sim_D_A))
                     # papers used in expertise
                     papers_of_expertise[a] = [p_id]
-                    score_authors_dict[a] = math.exp(sim_D_A * sim_Q_D)
+                    score_authors_dict[a] = math.exp(sim_Q_D / sim_D_A)
                     # score_authors_dict[a] = math.exp(sim_Q_D)
         else:
-            pass
+            not_found += 1
             
       
     # I comment this for optisation ( sort only one time outside tis fct)
@@ -725,7 +893,7 @@ def get_scores(df_res, papers,authors, dist_score_cluster = False,
                 
     return   score_authors_dict, papers_of_expertise
     
-
+# papers = papers[['id_paper', 'authors', 'list_authors_name'] ]
 
 def get_relevant_experts_multi_index_v2(queries, list_index_path, papers, 
                                      authors, embedder, 
@@ -736,8 +904,8 @@ def get_relevant_experts_multi_index_v2(queries, list_index_path, papers,
     
     
     
-    
-    
+    global not_found
+    not_found = 0
     
     rel_docs_each_query = {}
     final_scores = {}
@@ -783,21 +951,79 @@ def get_relevant_experts_multi_index_v2(queries, list_index_path, papers,
          # delete this index for optimization   
         del(sen_index)
         
+        
     
     # sort the result and select top k
     print("got results from every index")
+    if dist_score_cluster == True:
+        with open("dependencies/loc_embedding.pkl", "rb") as f:
+            loc_embs = pickle.load(f)
+            
+            
+        
+        # load the profil dict
+        with open("dependencies/profile_file", "rb") as f:
+            profiles = pickle.load(f)
+   
+    else:
+        loc_embs = None
+        profiles = None
+        
+    qi = 1
+    qt = len(queries)
     for q in queries:
+        print("query","*"*5,"[{}/{}]".format(qi,qt))
+        qi+=1
+        
+        
         
         rel_docs_each_query[q].sort_values(ascending=False, inplace=True)
     
+        #print("shape before : ",rel_docs_each_query[q].shape)
         rel_docs_each_query[q] = rel_docs_each_query[q].iloc[:k]
+        #print("shape before : ",rel_docs_each_query[q].shape)
+        # determine the embedding of each doc
+        
+        df_embs = read_emb(rel_docs_each_query[q], loc_embs)
     
-    
-        final_scores[q], papers_of_expertise[q] = get_scores(rel_docs_each_query[q], papers, authors,dist_score_cluster)
+        final_scores[q], papers_of_expertise[q] = get_scores(rel_docs_each_query[q], papers, 
+                                                             authors,dist_score_cluster, 
+                                                             profiles = profiles, loc_embs = loc_embs, df_embs = df_embs)
                    
         
         
     return   final_scores, papers_of_expertise
+    
+    
+    
+def read_emb(res_query, loc_file):
+    
+    global s_loc
+    
+    print("processing query...")
+    s_loc={}
+    for p_id in res_query.index:
+        if p_id in loc_file.keys():
+            nb = loc_file[p_id]
+        
+            if nb in s_loc.keys():
+                s_loc[nb].append(p_id)
+            else:
+                s_loc[nb] = [p_id]
+
+    
+    df_res = pd.DataFrame(columns=['id_paper', 'mean_embedding'])
+    
+    for nb in s_loc.keys():
+        #print("new df: ",nb)
+        fn = "dependencies/docs_embedding/emb{}.csv".format(nb)
+        df = pd.read_csv(fn)
+        
+        df_res = df_res.append(df.loc[df.id_paper.isin(s_loc[nb])])
+    
+    return df_res
+    
+    
     
     
     
